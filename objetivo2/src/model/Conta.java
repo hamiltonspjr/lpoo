7da7bf7642ea673ec053bc838fb71070117a50b9
Hamilton Junior <hamilton-souza@outlook.com.br>
2023-04-01 19:08:27 -0300

    created class Conta

---
+++ objetivo2/src/model/Conta.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Conta {
+    protected double saldo;
+
+    public Conta() {
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public Conta(double saldo) {
+        this.saldo = saldo;
+    }
+    public void deposita(double valor) {
+        this.saldo += valor;
+    }
+    public void saca(double valor) {
+        double newSaldo = this.saldo - valor;
+        if(newSaldo >= 0) {
+            this.saldo -= valor;
+        } else {
+            System.out.println("Não foi possível fazer esse saque!");
+        }
+    }
+    public void atualiza(double taxa) {
+        if(taxa > 0) {
+            this.saldo += this.saldo * (taxa/100);
+        } else {
+            System.out.println("A taxa deve ser maior que zero\n");
+        }
+    }
+}
