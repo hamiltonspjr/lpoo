754ff8f88019da4d9231718abcb84253b0405eb8
hamiltonspjr <hamilton-souza@outlook.com.br>
2023-05-14 19:47:31 -0300

    feat: completed class item

--- objetivo3E4/src/model/Item.java
+++ objetivo3E4/src/model/Item.java
@@ -4,4 +4,57 @@ public class Item {
     private Integer codItem;
     private Double desconto;
     private Integer quantidade;
+    Produto produto;
+
+    public Item() {
+    }
+
+    public Item(Integer codItem, Double desconto, Integer quantidade, Produto produto) {
+        this.codItem = codItem;
+        this.desconto = desconto;
+        this.quantidade = quantidade;
+        this.produto = produto;
+    }
+
+    public Integer getCodItem() {
+        return codItem;
+    }
+
+    public void setCodItem(Integer codItem) {
+        this.codItem = codItem;
+    }
+
+    public Double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(Double desconto) {
+        this.desconto = desconto;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    @Override
+    public String toString() {
+        return "\nItem{" +
+                "codItem=" + codItem +
+                ", desconto=" + desconto +
+                ", quantidade=" + quantidade +
+                ", produto=" + produto +
+                '}';
+    }
 }
