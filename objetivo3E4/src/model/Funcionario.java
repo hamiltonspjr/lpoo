d42c5966b5dab1b0041c83dec1b9cb30abff7882
hamiltonspjr <hamilton-souza@outlook.com.br>
2023-05-14 19:21:36 -0300

    feat:finished employee class

--- objetivo3E4/src/model/Funcionario.java
+++ objetivo3E4/src/model/Funcionario.java
@@ -1,7 +1,7 @@
 package model;
 
 public class Funcionario {
-    private int matricula;
+    private Integer matricula;
     private String nome;
     private String endereco;
     private String bairro;
@@ -12,13 +12,69 @@ public class Funcionario {
     public Funcionario() {
     }
 
-    public Funcionario(int matricula, String nome, String endereco, String bairro, String cep, String cidade, String estado) {
+    public Funcionario(Integer matricula, String nome, String endereco, String bairro, String cep, String cidade, String estado) {
         this.matricula = matricula;
         this.nome = nome;
         this.endereco = endereco;
         this.bairro = bairro;
         this.cep = cep;
         this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    public Integer getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Integer matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
         this.estado = estado;
     }
 
