67198008c710aaf891ba9b9ac5b61791996d5c59
hamiltonspjr <hamilton-souza@outlook.com.br>
2023-05-14 19:37:21 -0300

    feat: completed class request

--- objetivo3E4/src/model/Pedido.java
+++ objetivo3E4/src/model/Pedido.java
@@ -12,4 +12,57 @@ public class Pedido {
     public Pedido() {
     }
 
+    public Pedido(Integer numero, Calendar data, Double valor) {
+        this.numero = numero;
+        this.data = data;
+        this.valor = valor;
+    }
+
+    public Pedido(Integer numero, Calendar data, Double valor, Vendedor vendedor) {
+        this.numero = numero;
+        this.data = data;
+        this.valor = valor;
+        this.vendedor = vendedor;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public Calendar getData() {
+        return data;
+    }
+
+    public void setData(Calendar data) {
+        this.data = data;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Vendedor getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(Vendedor vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    @Override
+    public String toString() {
+        return "\nPedido{" +
+                "numero=" + numero +
+                ", data=" + data +
+                ", valor=" + valor +
+                '}';
+    }
 }
