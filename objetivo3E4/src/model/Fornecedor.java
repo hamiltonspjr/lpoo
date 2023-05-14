ae49ad55630c4ed28210495c856ed0613890b07b
hamiltonspjr <hamilton-souza@outlook.com.br>
2023-05-14 20:00:54 -0300

    completed supplier and supply class

--- objetivo3E4/src/model/Fornecedor.java
+++ objetivo3E4/src/model/Fornecedor.java
@@ -1,17 +1,55 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fornecedor {
-    private int cnpj;
+    private Integer cnpj;
     private String contato;
     private String nome;
+
+    private List<Produto> produtos = new ArrayList<>();
 
     public Fornecedor() {
     }
 
-    public Fornecedor(int cnpj, String contato, String nome) {
+    public Fornecedor(Integer cnpj, String contato, String nome, List<Produto> produtos) {
         this.cnpj = cnpj;
         this.contato = contato;
         this.nome = nome;
+        this.produtos = produtos;
+    }
+
+    public Integer getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(Integer cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
     }
 
     @Override
